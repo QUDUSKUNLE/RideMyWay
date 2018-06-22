82307b9407a0b3f418a55a80c39e3888ac10eda2
abdulquddus <qudus.yekeen@andela.com>
2018-06-22 14:55:00 +0100

    set up postgres on .travis.yml

---
+++ start_app.sh
@@ -0,0 +1,7 @@
+
+
+# run sh start_app.sh to start the application
+
+echo "****************************** App running ***********************************************"
+cd server
+python3 manage.py runserver 5000
